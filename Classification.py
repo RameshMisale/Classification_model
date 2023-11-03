289901922c4a8e1881f6916de1f9b88b34c23355
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-03 13:32:40 +0530

    Create Classification.py

---
+++ Classification.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+st.title("House Price Prediction")
+
+# Create input fields for user input
+st.sidebar.header('Input Features')
+age = st.sidebar.number_input("Age")
+bmi = st.sidebar.number_input("BMI")
+glucose = st.sidebar.number_input("Glucose")
+insulin = st.sidebar.number_input("Insulin")
+homa = st.sidebar.number_input("HOMA")
+leptin = st.sidebar.number_input("Leptin")
+adiponectin = st.sidebar.number_input("Adiponectin")
+resistin = st.sidebar.number_input("Resistin")
+mcp1 = st.sidebar.number_input("MCP.1")
+
+# Load your pre-trained regression model using pickle
+with open('Classification_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+# Create a DataFrame with the input data
+input_data = pd.DataFrame({
+    'Age': [age],
+    'BMI': [bmi],
+    'Glucose': [glucose],
+    'Insulin': [insulin],
+    'HOMA': [homa],
+    'Leptin': [leptin],
+    'Adiponectin': [adiponectin],
+    'Resistin': [resistin],
+    'MCP.1': [mcp1]
+})
+
+if st.sidebar.button("Predict Price"):
+    # Make the prediction
+    predicted_price = model.predict(input_data)[0]
+    st.write(f"Predicted Price: ${predicted_price:,.2f}")
