82d9491ba3da2ff2ff05045b7328510fb087561a
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-03 13:45:42 +0530

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -4,17 +4,21 @@ import pandas as pd
 
 st.title("Classification")
 
-# Create input fields for user input
-st.sidebar.header('Input Features')
-age = st.sidebar.number_input("Age")
-bmi = st.sidebar.number_input("BMI")
-glucose = st.sidebar.number_input("Glucose")
-insulin = st.sidebar.number_input("Insulin")
-homa = st.sidebar.number_input("HOMA")
-leptin = st.sidebar.number_input("Leptin")
-adiponectin = st.sidebar.number_input("Adiponectin")
-resistin = st.sidebar.number_input("Resistin")
-mcp1 = st.sidebar.number_input("MCP.1")
+# Center the input features under the title
+col1, col2 = st.beta_columns(2)
+
+# Create input fields for user input in the first column
+with col1:
+    st.header('Input Features')
+    age = st.number_input("Age")
+    bmi = st.number_input("BMI")
+    glucose = st.number_input("Glucose")
+    insulin = st.number_input("Insulin")
+    homa = st.number_input("HOMA")
+    leptin = st.number_input("Leptin")
+    adiponectin = st.number_input("Adiponectin")
+    resistin = st.number_input("Resistin")
+    mcp1 = st.number_input("MCP.1")
 
 # Load your pre-trained classification model using pickle
 with open('Classification_model.pkl', 'rb') as model_file:
@@ -33,7 +37,9 @@ input_data = pd.DataFrame({
     'MCP.1': [mcp1]
 })
 
-if st.sidebar.button("Predict"):
-    # Make the prediction
-    predicted_class = model.predict(input_data)[0]
-    st.write(f"Prediction: {predicted_class}")
+# Center the prediction under the input features
+with col2:
+    if st.button("Predict"):
+        # Make the prediction
+        predicted_class = model.predict(input_data)[0]
+        st.write(f"Prediction: {predicted_class}")
